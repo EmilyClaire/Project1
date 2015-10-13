d9e5cb5032e20a33e66f3e68efc23e5e43e07adf
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2015-10-13 13:39:30 -0700

    Adding minimally implemented Converter

---
+++ Converter.java
@@ -0,0 +1,33 @@
+/**
+ * A class that converts arithmetic expressions
+ * from infix to postfix notation.
+ *
+ * @author Emily Lucas (eclucas)
+ *
+ * @version Project 1 2015-10-08
+ */
+
+public class Converter 
+{
+    /**
+     * Converts a correctly formatted infix arithmetic expression into postfix notation.
+     * 
+     * @param expression the arithmetic expression to be converted
+     * @return expression in postfix notation
+     */
+    public static String infixToPostfix(String expression)
+    {
+        return null;
+    }
+    
+    /**
+     * Returns the value of a valid postfix arithmetic expression
+     * 
+     * @param valid postfix arithmetic expression to be evaluated
+     * @return result of the arithmetic expression
+     */
+    public static double postfixValue(String expression)
+    {
+        return Double.NaN;
+    }
+}
